b093a7ce56050a5ad0291957399ec81946f29d6f
koji <koji4386@gmail.com>
2021-04-25 19:25:48 +0200

    add movie details

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,25 @@ router.get('/movies', (req, res, next) => {
 
   Movie.find({})
     .then(movies => {
-      console.log('All movies:', movies);
+      // console.log('All movies:', movies);
       res.render('movies', { movies }); 
     })
     .catch(error => {
       console.log('Error while getting the movies from the DB: ', error);
+      next(error);
+    });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Movie.findById(id)
+    .then(movie => {
+      console.log('Details of the movie:', movie);
+      res.render('movie-detail', {movie})
+    })
+    .catch(error => {
+      console.log('Error while retrieving movie details', error);
       next(error);
     });
 });

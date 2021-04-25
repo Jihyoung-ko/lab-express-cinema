d53bfea17797d4112a92812fd4ba40aff79739d6
koji <koji4386@gmail.com>
2021-04-25 18:39:35 +0200

    list all the movies done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model.js'); 
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find({})
+    .then(movies => {
+      console.log('All movies:', movies);
+      res.render('movies', { movies }); 
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+      next(error);
+    });
+});
 
 module.exports = router;
